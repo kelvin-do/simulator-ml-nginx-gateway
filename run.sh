eb993639b6f68e962d76c88dd56fc7d579bc5259
a-cure <curealexis8@msn.com>
2025-03-13 10:27:00 +0100

    Fix TR issue replacing it with cut

--- run.sh
+++ run.sh
@@ -13,14 +13,14 @@ cp /etc/nginx/routes.conf.template /etc/nginx/routes.conf
 
 fqdn=$(grep search /etc/resolv.conf | awk '{print $2}' | head -1)
 
-if [ -z "$fqdn" ]; then
+if [ -z "$fqdn" ] || [ "$fqdn" = "." ]; then
   fqdn='local'
 fi
 
 for var in $(env)
 do
   ENV_VAR_KEY="\$$(echo "$var" | tr "=" " " | awk '{print $1}')"
-  ENV_VAR_VALUE=$(echo "$var" | tr "$ENV_VAR_KEY=" " " | awk '{print $1}')
+  ENV_VAR_VALUE=$(echo "$var" | cut -d= -f2-)
 
   case "$ENV_VAR_VALUE" in
   *.*)
